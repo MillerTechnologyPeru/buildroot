9728357fb5f3baf401a5987e87697d46a3bc2889
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-08-24 23:38:45 -0500

    Working on Swift

--- package/swift/swift.mk
+++ package/swift/swift.mk
@@ -12,13 +12,12 @@ ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
 SWIFT_CONF_ENV += LIBS="-latomic"
 endif
 
-SWIFT_TARGET_FLAGS="--sysroot=${STAGING_DIR_TARGET} -target $(GNU_TARGET_NAME)"
-EXTRA_INCLUDE_FLAGS="-I${STAGING_DIR_TARGET}/usr/include/c++/${SWIFT_CXX_VERSION} -I${STAGING_DIR_TARGET}/usr/include"
-RUNTIME_FLAGS="-w -fuse-ld=lld ${TARGET_FLAGS} -B${STAGING_DIR_TARGET}/usr/lib -B${STAGING_DIR_TARGET}/lib"
-LINK_FLAGS="${TARGET_FLAGS}"
+SWIFT_TARGET_FLAGS=--sysroot=$(STAGING_DIR) -target $(GNU_TARGET_NAME)
+SWIFT_EXTRA_INCLUDE_FLAGS=-I$(STAGING_DIR)/usr/include
+SWIFT_RUNTIME_FLAGS=-w -fuse-ld=lld $(SWIFT_TARGET_FLAGS) -B$(STAGING_DIR)/usr/lib -B$(STAGING_DIR)/lib -B$(HOST_DIR)/lib/gcc/aarch64-buildroot-linux-gnu/10.3.0
+SWIFT_LINK_FLAGS=$(SWIFT_TARGET_FLAGS)
 
 SWIFT_CONF_OPTS +=  \
-    -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON \
     -DSWIFT_USE_LINKER=lld \
     -DLLVM_USE_LINKER=lld \
     -DCMAKE_C_COMPILER=$(SWIFT_NATIVE_PATH)/usr/bin/clang \
@@ -40,6 +39,8 @@ SWIFT_CONF_OPTS +=  \
     -DSWIFT_INCLUDE_DOCS=OFF \
     -DSWIFT_INCLUDE_TOOLS=OFF \
     -DSWIFT_INCLUDE_TESTS=OFF \
+    -DSWIFT_LIBRARY_EVOLUTION=0 \
+    -DSWIFT_RUNTIME_OS_VERSIONING=OFF \
     -DSWIFT_HOST_VARIANT_ARCH=$(SWIFT_TARGET_ARCH) \
     -DSWIFT_SDKS=LINUX \
     -DSWIFT_SDK_LINUX_ARCH_$(SWIFT_TARGET_ARCH)_PATH=${STAGING_DIR}  \
@@ -54,6 +55,31 @@ ifeq (SWIFT_SUPPORTS_IN_SOURCE_BUILD),YES)
 SWIFT_BUILDDIR			= $(SWIFT_SRCDIR)
 else
 SWIFT_BUILDDIR			= $(SWIFT_SRCDIR)/buildroot-build
+endif
+
+# CMAKE_SYSTEM_PROCESSOR should match uname -m
+ifeq ($(BR2_ARM_CPU_ARMV4),y)
+CMAKE_SYSTEM_PROCESSOR_ARM_VARIANT = armv4
+else ifeq ($(BR2_ARM_CPU_ARMV5),y)
+CMAKE_SYSTEM_PROCESSOR_ARM_VARIANT = armv5
+else ifeq ($(BR2_ARM_CPU_ARMV6),y)
+CMAKE_SYSTEM_PROCESSOR_ARM_VARIANT = armv6
+else ifeq ($(BR2_ARM_CPU_ARMV7A),y)
+CMAKE_SYSTEM_PROCESSOR_ARM_VARIANT = armv7
+else ifeq ($(BR2_ARM_CPU_ARMV8A),y)
+CMAKE_SYSTEM_PROCESSOR_ARM_VARIANT = armv8
+endif
+
+ifeq ($(BR2_arm),y)
+CMAKE_SYSTEM_PROCESSOR = $(CMAKE_SYSTEM_PROCESSOR_ARM_VARIANT)l
+else ifeq ($(BR2_armeb),y)
+CMAKE_SYSTEM_PROCESSOR = $(CMAKE_SYSTEM_PROCESSOR_ARM_VARIANT)b
+else ifeq ($(call qstrip,$(BR2_ARCH)),powerpc64)
+CMAKE_SYSTEM_PROCESSOR = ppc64
+else ifeq ($(call qstrip,$(BR2_ARCH)),powerpc64le)
+CMAKE_SYSTEM_PROCESSOR = ppc64le
+else
+CMAKE_SYSTEM_PROCESSOR = $(BR2_ARCH)
 endif
 
 define SWIFT_CONFIGURE_CMDS
@@ -72,44 +98,43 @@ define SWIFT_CONFIGURE_CMDS
 		-DBUILD_TESTS=OFF \
 		-DBUILD_TESTING=OFF \
 		-DBUILD_SHARED_LIBS=ON \
+		-DCMAKE_BUILD_TYPE=$(if $(BR2_ENABLE_RUNTIME_DEBUG),Debug,Release) \
+    		-DCMAKE_C_FLAGS="$(SWIFT_RUNTIME_FLAGS) $(SWIFT_EXTRA_INCLUDE_FLAGS)" \
+    		-DCMAKE_CXX_FLAGS="$(SWIFT_RUNTIME_FLAGS) $(SWIFT_EXTRA_INCLUDE_FLAGS)" \
+    		-DCMAKE_C_LINK_FLAGS="$(SWIFT_LINK_FLAGS)" \
+    		-DCMAKE_CXX_LINK_FLAGS="$(SWIFT_LINK_FLAGS)" \
+    		-DCMAKE_SYSTEM_PROCESSOR=$(CMAKE_SYSTEM_PROCESSOR) \
 		$(SWIFT_CONF_OPTS) \
 	)
 endef
 
 define SWIFT_BUILD_CMDS
-	cd $(SWIFT_BUILDDIR)
-	ninja
+	(cd $(SWIFT_BUILDDIR) && ninja)
 endef
 
 define SWIFT_INSTALL_TARGET_CMDS
-	cd $(SWIFT_BUILDDIR)
-	mkdir $(TARGET_DIR)/usr/lib/swift
-	mkdir $(TARGET_DIR)/usr/lib/swift/linux
-
-	cp ./lib/swift/linux/libswiftCore.so $(TARGET_DIR)/usr/lib
-	cp ./lib/swift/linux/libswiftRemoteMirror.so $(TARGET_DIR)/usr/lib
-	cp ./lib/swift/linux/libswiftSwiftOnoneSupport.so $(TARGET_DIR)/usr/lib
+	(cd $(SWIFT_BUILDDIR) && \
+	cp ./lib/swift/linux/libswiftCore.so $(TARGET_DIR)/usr/lib && \
+	cp ./lib/swift/linux/libswiftRemoteMirror.so $(TARGET_DIR)/usr/lib && \
+	cp ./lib/swift/linux/libswiftSwiftOnoneSupport.so $(TARGET_DIR)/usr/lib \
+	)
 endef
 
 define SWIFT_INSTALL_STAGING_CMDS
-
-	cd $(SWIFT_BUILDDIR)
-
-	mkdir ${STAGING_DIR}/usr/lib/swift
-	mkdir ${STAGING_DIR}/usr/lib/swift/linux
-
-	cp -rf ./lib/swift/shims ${STAGING_DIR}/usr/lib/swift/
-
-	cp ./lib/swift/linux/libswiftCore.so ${STAGING_DIR}/usr/lib/swift/linux/
-	cp ./lib/swift/linux/libswiftRemoteMirror.so ${STAGING_DIR}/usr/lib/swift/linux/
-	cp ./lib/swift/linux/libswiftSwiftOnoneSupport.so ${STAGING_DIR}/usr/lib/swift/linux/
-	cp -rf ./lib/swift/linux/Glibc.swiftmodule ${STAGING_DIR}/usr/lib/swift/linux/
-	cp -rf ./lib/swift/linux/Swift.swiftmodule ${STAGING_DIR}/usr/lib/swift/linux/
-	cp -rf ./lib/swift/linux/SwiftOnoneSupport.swiftmodule ${STAGING_DIR}/usr/lib/swift/linux/
-
-	mkdir ${STAGING_DIR}/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH)
-	cp ./lib/swift/linux/$(SWIFT_TARGET_ARCH)/glibc.modulemap ${STAGING_DIR}/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH)/
-	cp ./lib/swift/linux/$(SWIFT_TARGET_ARCH)/swiftrt.o ${STAGING_DIR}/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH)/
+	(cd $(SWIFT_BUILDDIR) && \
+	mkdir ${STAGING_DIR}/usr/lib/swift && \
+	mkdir ${STAGING_DIR}/usr/lib/swift/linux && \
+	cp -rf ./lib/swift/shims ${STAGING_DIR}/usr/lib/swift/ && \
+	cp ./lib/swift/linux/libswiftCore.so ${STAGING_DIR}/usr/lib/swift/linux/ && \
+	cp ./lib/swift/linux/libswiftRemoteMirror.so ${STAGING_DIR}/usr/lib/swift/linux/ && \
+	cp ./lib/swift/linux/libswiftSwiftOnoneSupport.so ${STAGING_DIR}/usr/lib/swift/linux/ && \
+	cp -rf ./lib/swift/linux/Glibc.swiftmodule ${STAGING_DIR}/usr/lib/swift/linux/ && \
+	cp -rf ./lib/swift/linux/Swift.swiftmodule ${STAGING_DIR}/usr/lib/swift/linux/ && \
+	cp -rf ./lib/swift/linux/SwiftOnoneSupport.swiftmodule ${STAGING_DIR}/usr/lib/swift/linux/ && \
+	mkdir ${STAGING_DIR}/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH) && \
+	cp ./lib/swift/linux/$(SWIFT_TARGET_ARCH)/glibc.modulemap ${STAGING_DIR}/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH)/ && \
+	cp ./lib/swift/linux/$(SWIFT_TARGET_ARCH)/swiftrt.o ${STAGING_DIR}/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH)/ \
+	)
 endef
 
 $(eval $(generic-package))
