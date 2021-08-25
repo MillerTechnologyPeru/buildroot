59732a9cfeb1f1050bd9370dbfc2909986c2ebc0
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-08-24 21:58:00 -0500

    Working on Swift package

--- package/swift/swift.mk
+++ package/swift/swift.mk
@@ -1,16 +1,21 @@
 ### Apple's Swift Programming Language
-SWIFT_VERSION = 5.4.2
+SWIFT_VERSION =  $(call qstrip,$(BR2_PACKAGE_SWIFT_VERSION))
+SWIFT_TARGET_ARCH = $(call qstrip,$(BR2_PACKAGE_SWIFT_TARGET_ARCH))
+SWIFT_NATIVE_PATH = $(call qstrip,$(BR2_PACKAGE_SWIFT_NATIVE_TOOLS))
 SWIFT_SOURCE = swift-$(SWIFT_VERSION)-RELEASE.tar.gz
 SWIFT_SITE = https://github.com/apple/swift/archive/refs/tags
 SWIFT_INSTALL_STAGING = YES
 SWIFT_INSTALL_TARGET = YES
 SWIFT_SUPPORTS_IN_SOURCE_BUILD = NO
-SWIFT_DEPENDENCIES = llvm icu
-SWIFT_TARGET_ARCH = $(BR2_PACKAGE_SWIFT_TARGET_ARCH)
-SWIFT_NATIVE_PATH = /home/coleman/Downloads/swift-5.4.2-RELEASE-ubuntu20.04
+SWIFT_DEPENDENCIES = icu
 ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
 SWIFT_CONF_ENV += LIBS="-latomic"
 endif
+
+SWIFT_TARGET_FLAGS="--sysroot=${STAGING_DIR_TARGET} -target $(GNU_TARGET_NAME)"
+EXTRA_INCLUDE_FLAGS="-I${STAGING_DIR_TARGET}/usr/include/c++/${SWIFT_CXX_VERSION} -I${STAGING_DIR_TARGET}/usr/include"
+RUNTIME_FLAGS="-w -fuse-ld=lld ${TARGET_FLAGS} -B${STAGING_DIR_TARGET}/usr/lib -B${STAGING_DIR_TARGET}/lib"
+LINK_FLAGS="${TARGET_FLAGS}"
 
 SWIFT_CONF_OPTS +=  \
     -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON \
@@ -18,8 +23,8 @@ SWIFT_CONF_OPTS +=  \
     -DLLVM_USE_LINKER=lld \
     -DCMAKE_C_COMPILER=$(SWIFT_NATIVE_PATH)/usr/bin/clang \
     -DCMAKE_CXX_COMPILER=$(SWIFT_NATIVE_PATH)/usr/bin/clang++ \
-    -DLLVM_DIR=${HOST_DIR}/lib/cmake/llvm \
-    -DLLVM_BUILD_LIBRARY_DIR=${HOST_DIR}/lib \
+    -DLLVM_DIR=/usr/lib/llvm-10/lib/cmake/llvm \
+    -DLLVM_BUILD_LIBRARY_DIR=/usr/lib/llvm-10 \
     -DSWIFT_BUILD_RUNTIME_WITH_HOST_COMPILER=ON \
     -DSWIFT_NATIVE_CLANG_TOOLS_PATH=$(SWIFT_NATIVE_PATH)/usr/bin \
     -DSWIFT_NATIVE_SWIFT_TOOLS_PATH=$(SWIFT_NATIVE_PATH)/usr/bin \
@@ -45,4 +50,66 @@ SWIFT_CONF_OPTS +=  \
     -DICU_I18N_LIBRARIES=${STAGING_DIR}/usr/lib/libicui18n.so \
     -DICU_UC_LIBRARIES=${STAGING_DIR}/usr/lib/libicuuc.so \
 
-$(eval $(cmake-package))
+ifeq (SWIFT_SUPPORTS_IN_SOURCE_BUILD),YES)
+SWIFT_BUILDDIR			= $(SWIFT_SRCDIR)
+else
+SWIFT_BUILDDIR			= $(SWIFT_SRCDIR)/buildroot-build
+endif
+
+define SWIFT_CONFIGURE_CMDS
+	(mkdir -p $(SWIFT_BUILDDIR) && \
+	cd $(SWIFT_BUILDDIR) && \
+	rm -f CMakeCache.txt && \
+	PATH=$(BR_PATH) \
+	$(SWIFT_CONF_ENV) $(BR2_CMAKE) -S $(SWIFT_SRCDIR) -B $(SWIFT_BUILDDIR) -G Ninja \
+		-DCMAKE_INSTALL_PREFIX="/usr" \
+		-DCMAKE_COLOR_MAKEFILE=OFF \
+		-DBUILD_DOC=OFF \
+		-DBUILD_DOCS=OFF \
+		-DBUILD_EXAMPLE=OFF \
+		-DBUILD_EXAMPLES=OFF \
+		-DBUILD_TEST=OFF \
+		-DBUILD_TESTS=OFF \
+		-DBUILD_TESTING=OFF \
+		-DBUILD_SHARED_LIBS=ON \
+		$(SWIFT_CONF_OPTS) \
+	)
+endef
+
+define SWIFT_BUILD_CMDS
+	cd $(SWIFT_BUILDDIR)
+	ninja
+endef
+
+define SWIFT_INSTALL_TARGET_CMDS
+	cd $(SWIFT_BUILDDIR)
+	mkdir $(TARGET_DIR)/usr/lib/swift
+	mkdir $(TARGET_DIR)/usr/lib/swift/linux
+
+	cp ./lib/swift/linux/libswiftCore.so $(TARGET_DIR)/usr/lib
+	cp ./lib/swift/linux/libswiftRemoteMirror.so $(TARGET_DIR)/usr/lib
+	cp ./lib/swift/linux/libswiftSwiftOnoneSupport.so $(TARGET_DIR)/usr/lib
+endef
+
+define SWIFT_INSTALL_STAGING_CMDS
+
+	cd $(SWIFT_BUILDDIR)
+
+	mkdir ${STAGING_DIR}/usr/lib/swift
+	mkdir ${STAGING_DIR}/usr/lib/swift/linux
+
+	cp -rf ./lib/swift/shims ${STAGING_DIR}/usr/lib/swift/
+
+	cp ./lib/swift/linux/libswiftCore.so ${STAGING_DIR}/usr/lib/swift/linux/
+	cp ./lib/swift/linux/libswiftRemoteMirror.so ${STAGING_DIR}/usr/lib/swift/linux/
+	cp ./lib/swift/linux/libswiftSwiftOnoneSupport.so ${STAGING_DIR}/usr/lib/swift/linux/
+	cp -rf ./lib/swift/linux/Glibc.swiftmodule ${STAGING_DIR}/usr/lib/swift/linux/
+	cp -rf ./lib/swift/linux/Swift.swiftmodule ${STAGING_DIR}/usr/lib/swift/linux/
+	cp -rf ./lib/swift/linux/SwiftOnoneSupport.swiftmodule ${STAGING_DIR}/usr/lib/swift/linux/
+
+	mkdir ${STAGING_DIR}/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH)
+	cp ./lib/swift/linux/$(SWIFT_TARGET_ARCH)/glibc.modulemap ${STAGING_DIR}/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH)/
+	cp ./lib/swift/linux/$(SWIFT_TARGET_ARCH)/swiftrt.o ${STAGING_DIR}/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH)/
+endef
+
+$(eval $(generic-package))
