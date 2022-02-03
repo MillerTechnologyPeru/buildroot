5a50dd1b1518d711d5d8a2f81d1897ad2ac4a37a
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-02-02 19:20:38 -0800

    Updated hello-swift

--- package/swift-hello/src/Sources/swift-hello/main.swift
+++ package/swift-hello/src/Sources/swift-hello/main.swift
@@ -2,13 +2,19 @@ import Foundation
 import Dispatch
 import Glibc
 
-func main() {
+func main() async throws {
     print("Hello, world! 👋")
+    try await Task.sleep(1_000_000_000)
     print("User: \(ProcessInfo.processInfo.fullUserName)")
     let dateFormatter = DateFormatter()
     dateFormatter.dateStyle = .full
     dateFormatter.timeStyle = .medium
     print(dateFormatter.string(from: Date()))
+    throw URLError(.unknown)
 }
 
-main()
+let task = Task {
+    try await main()
+}
+
+RunLoop.main.run(until: Date() + 2)
